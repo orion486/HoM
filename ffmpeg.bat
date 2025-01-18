3d2d805a7d88f90b363d7b1098343120c8ccf156
orion486 <knottingham@yahoo.com>
2025-01-18 15:17:08 -0800

    merges two ffmpeg calls into a single one to save processing time and memory space

--- ffmpeg.bat
+++ ffmpeg.bat
@@ -1,3 +1,9 @@
-ffmpeg -framerate 60 -i img%%04d.bmp -c:v libx264rgb -crf 0 -qp 0 output.mp4
-ffmpeg -i output.mp4 -i hom.mp3 -c copy -map 0:v:0 -map 1:a:0 hom.mp4
+ffmpeg ^
+-framerate 60 ^
+-i img%%04d.bmp ^
+-i hom.mp3 ^
+-c:v libx264rgb -crf 0 -qp 0 ^
+-c:a copy ^
+hom.mp4
+
 pause
